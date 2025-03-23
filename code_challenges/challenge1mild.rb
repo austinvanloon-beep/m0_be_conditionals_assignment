9b97d50a79173c272bab5aee885789a9a89e46fa
Austin Vanloon <wic404learn@gmail.com>
2025-03-23 12:38:40 -0600

    code challenge 1 mild

---
+++ code_challenges/challenge1mild.rb
@@ -0,0 +1,20 @@
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+puts "Write a Ruby program that defines a variable that stores an integer. The program should print out the string even if the integer is even, and the string odd if the integer is odd."
+
+number = 8
+
+if number.even?
+  puts "even"
+else
+  puts "odd"
+end
+
+#-------------------
+
+number = 3
+if number.even?
+  puts "even"
+else
+  puts "odd"
+end
